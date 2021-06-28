4655987888cf072fb2310b1b9be6e77e7c675425
Ryan Gorgol <ryangorgol@gmail.com>
2021-06-28 12:37:09 -0700

    Updated game logic to alert user to memory failure! ie losing the game

--- src/Components/CardContainer.js
+++ src/Components/CardContainer.js
@@ -1,23 +1,23 @@
-import React, { useState, useEffect } from "react";
-import avocado from "../img/avocado.svg";
-import apple from "../img/apple-1.svg";
-import banana from "../img/banana.svg";
-import broccoli from "../img/broccoli.svg";
-import carrot from "../img/carrot.svg";
-import cheese from "../img/cheese-1.svg";
-import cherries from "../img/cherries.svg";
-import doughnut from "../img/doughnut-1.svg";
+import React, { useState } from 'react';
+import avocado from '../img/avocado.svg';
+import apple from '../img/apple-1.svg';
+import banana from '../img/banana.svg';
+import broccoli from '../img/broccoli.svg';
+import carrot from '../img/carrot.svg';
+import cheese from '../img/cheese-1.svg';
+import cherries from '../img/cherries.svg';
+import doughnut from '../img/doughnut-1.svg';
 
 function CardContainer(props) {
   const [food, setFood] = useState([
-    { img: apple, name: "apple", selected: false },
-    { img: avocado, name: "avocado", selected: false },
-    { img: banana, name: "banana", selected: false },
-    { img: broccoli, name: "broccoli", selected: false },
-    { img: carrot, name: "carrot", selected: false },
-    { img: cheese, name: "cheese", selected: false },
-    { img: cherries, name: "cherries", selected: false },
-    { img: doughnut, name: "doughnuts", selected: false },
+    { img: apple, name: 'apple', selected: false },
+    { img: avocado, name: 'avocado', selected: false },
+    { img: banana, name: 'banana', selected: false },
+    { img: broccoli, name: 'broccoli', selected: false },
+    { img: carrot, name: 'carrot', selected: false },
+    { img: cheese, name: 'cheese', selected: false },
+    { img: cherries, name: 'cherries', selected: false },
+    { img: doughnut, name: 'doughnuts', selected: false },
   ]);
 
   //shuffle food array function
@@ -40,17 +40,22 @@ function CardContainer(props) {
     return food;
   }
 
-  //setState of food array without re-rendering
-  useEffect(() => {
-    //  const shuffled = shuffle(food);
-    console.log(food);
-  }, [food]);
-
   //upon selecting card, setSelected to 'true'
-  const selectCard = (foodIndex) => {
+  const selectCard = (selected, foodIndex) => {
+    console.log(selected);
+    if (selected === true) {
+      alert('This card has already been chosen! Memory Failure');
+    } else {
+      console.log('so far so good');
+    }
+
     const newFood = food.map((foodItem, index) => {
       if (foodIndex === index) {
-        return { img: foodItem.img, name: foodItem.name, selected: true };
+        return {
+          img: foodItem.img,
+          name: foodItem.name,
+          selected: true,
+        };
       } else {
         return foodItem;
       }
@@ -60,15 +65,16 @@ function CardContainer(props) {
     setFood(shuffledFood);
 
     //check the 'selected' state of the card.
+
     //if 'selected' is true: game over, confirm high score.
     // if 'selected' is false: shuffle array, re-render and score++
   };
 
   return (
     <div className="CardContainer">
-      {food.slice(0, 4).map(({ img, name }, index) => (
-        <button onClick={() => selectCard(index)}>
-          <img src={img} alt={name}></img>
+      {food.slice(0, 4).map(({ img, name, selected }, index) => (
+        <button onClick={() => selectCard(selected, index)}>
+          <img src={img} alt={name} key={name}></img>
         </button>
       ))}
     </div>
